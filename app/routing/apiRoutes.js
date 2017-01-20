a3df5ff81bcb524243846f7f2667460d64f76a38
Emily Turner-Sherwood <emilyturnerr@gmail.com>
2017-01-20 16:27:33 -0600

    json showing woring on compatibility logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,19 @@ var surveyResults = require('../data/friends.js');
 var path = require ('path');
 var bodyParser = require ('body-parser');
 
+
+// API GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases when a user visits a link
+// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in surveyResults 
 module.exports = function(app) {
 	app.get('/api/friends', function(req, res) {
 		res.json(surveyResults);
 	});
-
+	app.post('/api/friends', function(req, res) {
+		var friendInfo = req.body;
+		console.log(req.body);
+	});
 
 };
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,8 +12,10 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname + "/../public/survey.html"));
 });
 
-//If no matching route is found, send the user to the Home Page - Default Page
-// app.use(function (req, res) {
-//   res.sendFile(path.join(__dirname + '/../public/home.html'));
-// });
+// If no matching route is found, send the user to the Home Page - Default Page
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname + '/../public/home.html'));
+});
 };
+
+
